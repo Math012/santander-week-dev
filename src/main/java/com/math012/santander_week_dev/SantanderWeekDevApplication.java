92faacef435a72d251333c088cec46fd416afeac
Math012 <math111.0811@gmail.com>
2025-02-12 17:03:56 -0300

    Initial commit

---
+++ src/main/java/com/math012/santander_week_dev/SantanderWeekDevApplication.java
@@ -0,0 +1,13 @@
+package com.math012.santander_week_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderWeekDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderWeekDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/math012/santander_week_dev/SantanderWeekDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math012.santander_week_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderWeekDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
