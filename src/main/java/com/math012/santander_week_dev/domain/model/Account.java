19e7ee3b678363527c73de5bd15e7f529637bb11
Math012 <math111.0811@gmail.com>
2025-02-13 15:57:47 -0300

    Alterando os campos scale e precision

--- src/main/java/com/math012/santander_week_dev/domain/model/Account.java
+++ src/main/java/com/math012/santander_week_dev/domain/model/Account.java
@@ -16,10 +16,10 @@ public class Account {
 
     private String agency;
 
-    @Column(scale = 13, precision = 2)
+    @Column(precision = 13, scale = 2)
     private BigDecimal balance;
 
-    @Column(name = "addition_limit", scale = 13, precision = 2)
+    @Column(name = "addition_limit", precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Account(){}
