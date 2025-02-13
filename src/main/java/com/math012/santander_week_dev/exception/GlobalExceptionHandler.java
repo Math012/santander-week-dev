599edcd641051fa93502ac564b76e5f4936fd0f3
Math012 <math111.0811@gmail.com>
2025-02-13 16:47:17 -0300

    alterando o nome dos metodos para handle<nome da excecao> e tratando a execao NoSuchElementException

--- src/main/java/com/math012/santander_week_dev/exception/GlobalExceptionHandler.java
+++ src/main/java/com/math012/santander_week_dev/exception/GlobalExceptionHandler.java
@@ -5,12 +5,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.NoSuchElementException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handle(IllegalArgumentException err){
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException err){
         return new ResponseEntity<>(err.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException err){
+        return new ResponseEntity<>("Resource id not found!", HttpStatus.NOT_FOUND);
     }
 
 }
